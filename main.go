37ebca655c5d481d9b656697b58f4d803b4c7868
dm1254 <d.moreno1272@gmail.com>
2025-02-08 19:48:32 -0800

    Add main.go: Initialize Pokedex CLI with API client and REPL

---
+++ main.go
@@ -0,0 +1,23 @@
+package main 
+import(
+	"strings"
+	"github.com/dm1254/pokedexcli/internal/pokeapi"
+	"time"
+	"math/rand"
+)
+func main(){
+	rand.Seed(time.Now().UnixNano())
+	pokeClient := pokeapi.NewClient(5 * time.Second, time.Minute*5)
+	cfg := &Config{
+		pokeapiClient: pokeClient,
+
+	}
+	startRepl(cfg)	
+}
+
+func cleanInput(text string) []string{	
+	lowerCase := strings.ToLower(text)
+	reformattedString := strings.TrimSpace(lowerCase)
+	splitTextToSlice := strings.Fields(reformattedString)
+	return splitTextToSlice
+}
