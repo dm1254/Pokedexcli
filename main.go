b0e3933ad3bb7b27628abfeb2a7918152768cae0
dm1254 <d.moreno1272@gmail.com>
2025-02-08 19:58:22 -0800

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main 
 import(
-	"strings"
 	"github.com/dm1254/pokedexcli/internal/pokeapi"
 	"time"
 	"math/rand"
@@ -15,3 +14,7 @@ func main(){
 	startRepl(cfg)	
 }
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> a9024b1 (Update main.go)
